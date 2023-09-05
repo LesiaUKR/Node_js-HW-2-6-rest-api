c5dfc4b8b846b4d5a0ea5572cb29b95a0c301b12
Your Name <solovyova.lesya@gmail.com>
2023-09-05 17:02:55 +0300

    deleted unnecessary files

--- models/contact.js
+++ models/contact.js
@@ -3,7 +3,8 @@ const { handleMongooseError } = require("../helpers");
 const Joi = require("joi");
 
 const nameRegexp = /^[\w\s'-]+$/;
-const phoneRegexp =/^((\+)?(3)?(8)?[- ]?)?(\(?\d{3}\)?[- ]?)?\d{3}[- ]?\d{2}[- ]?\d{2}$/;
+const phoneRegexp =
+  /^((\+)?(3)?(8)?[- ]?)?(\(?\d{3}\)?[- ]?)?\d{3}[- ]?\d{2}[- ]?\d{2}$/;
 
 const contactSchema = new Schema(
   {
@@ -57,8 +58,6 @@ const updateFavoriteSchema = Joi.object({
     .required()
     .messages({ "any.required": "Missing field favorite" }),
 });
-
-
 
 const schemas = {
   ContactAddSchema,
