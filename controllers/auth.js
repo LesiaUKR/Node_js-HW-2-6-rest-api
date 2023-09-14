551b55c00d975228339c5622ec4a6ab3dd5b0cc9
Your Name <solovyova.lesya@gmail.com>
2023-09-14 19:56:44 +0300

    added schemas and model for user auth

---
+++ controllers/auth.js
@@ -0,0 +1,22 @@
+const { User } = require("../models/user");
+
+const { ctrlWrapper } = require("../helpers");
+
+const register = async (req, res) => {
+   const { email } = req.body;
+   const user = await User.findOne({ email });
+   if (user) {
+   throw message ""
+}
+   const newUser = await User.create(req.body);
+
+   res.status(201).json({
+      email: newUser.email,
+      name: newUser.name,
+   })
+}
+
+
+module.exports = {
+   register:ctrlWrapper(register),
+}
