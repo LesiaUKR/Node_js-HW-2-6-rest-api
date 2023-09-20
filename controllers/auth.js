0ebd523a9a78e084b21421b2af5949d3826607ab
Your Name <solovyova.lesya@gmail.com>
2023-09-20 15:51:46 +0300

    added jimp

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,9 +3,9 @@ const jwt = require("jsonwebtoken");
 const gravatar = require("gravatar");
 const path = require("path");
 const fs = require("fs/promises");
+const Jimp = require("jimp");
 
 const { User } = require("../models/user");
-
 const { HttpError, ctrlWrapper } = require("../helpers");
 
 const { SECRET_KEY } = process.env;
@@ -105,11 +105,22 @@ const updateSubscription = async (req, res) => {
 const updateAvatar = async (req, res) => {
   const { _id } = req.user;
   const { path: tempUpload, originalname } = req.file;
+
+  await Jimp.read(tempUpload)
+    .then((avatar) => {
+      return avatar.resize(250, 250).quality(60).write(tempUpload);
+    })
+    .catch((err) => {
+      throw err;
+    });
+
   const fileName = `${_id}_${originalname}`;
   const resultUpload = path.join(avatarsDir, fileName);
 
   await fs.rename(tempUpload, resultUpload);
+
   const avatarURL = path.join("avatars", fileName);
+
   await User.findByIdAndUpdate(_id, { avatarURL });
 
   res.json({
